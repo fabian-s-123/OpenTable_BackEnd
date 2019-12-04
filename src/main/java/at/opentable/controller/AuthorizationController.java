108a6d0a55112bf6c3cc92ce84d6583f8b38e050
elnino07 <55378895+elnino07@users.noreply.github.com>
2019-12-04 19:10:30 +0100

    started jws implementation
    need to figure out what Costumer to use for "currentCustomer"

--- src/main/java/at/opentable/controller/AuthorizationController.java
+++ src/main/java/at/opentable/controller/AuthorizationController.java
@@ -9,24 +9,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 import java.util.Optional;
 
 @Controller
 public class AuthorizationController {
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256)
     @Autowired
     ModelMapper modelMapper;
     @Autowired
     CustomerRespository customerRespository;
 
     public ResponseEntity login(AuthenticationDTO authenticationDTO) {
+        // todo: AuthorizedCustomerDTO currentCustomer; ???
         Optional<Customer> customer = customerRespository.findByEmail(authenticationDTO.getEmail());
         if(customer.isPresent()) {
             if(authenticationDTO.getPassword().equals(customer.get().getPassword())) {
-                return  new ResponseEntity(modelMapper.map(customer.get(), AuthorizedCustomerDTO.class), HttpStatus.OK);
+                String jws = Jwts.builder().setSubject(customer.get().getEmail()).signWith(key).compact();
+                // todo: customer.setJws(jws);
+                return new ResponseEntity(modelMapper.map(customer.get(), AuthorizedCustomerDTO.class), HttpStatus.OK);
             }
         }
 
         return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    public boolean isAuthorized(String token) {
+        try {
+            String result = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getSubject();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
