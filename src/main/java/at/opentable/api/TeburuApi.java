8d078538f7c0e2a5a020b61abd788024e6fd1350
manzjan <55379246+manzjan@users.noreply.github.com>
2019-12-04 21:39:11 +0100

    restaurant CRUD

--- src/main/java/at/opentable/api/TeburuApi.java
+++ src/main/java/at/opentable/api/TeburuApi.java
@@ -3,6 +3,8 @@ package at.opentable.api;
 import at.opentable.controller.TeburuController;
 import at.opentable.entity.Teburu;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -34,4 +36,10 @@ public class TeburuApi {
         return Optional.of(teburuController.getTeburu(id));
     }
 
+    @DeleteMapping("/teburus/{id}")
+    public ResponseEntity deleteTeburu(@PathVariable int id) {
+        if(teburuController.deleteTeburu(id)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
 }
---
+++ src/main/java/at/opentable/controller/RestaurantController.java
@@ -0,0 +1,44 @@
+package at.opentable.controller;
+
+import at.opentable.entity.Restaurant;
+import at.opentable.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
+public class RestaurantController {
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    public void createRestaurant(Restaurant restaurant) {
+        restaurantRepository.save(restaurant);
+    }
+
+    public Optional updateRestaurant(Restaurant restaurant) {
+        Optional<Restaurant> optionalRestaurant = getRestaurant(restaurant.getId());
+        if (optionalRestaurant.isPresent()) {
+            restaurantRepository.save(restaurant);
+            return Optional.of(getRestaurant(restaurant.getId()));
+        }
+        return Optional.empty();
+    }
+
+    public Optional getRestaurant(int id) {
+        return Optional.of(restaurantRepository.findById(id));
+    }
+
+    public Iterable<Restaurant> findAll() {
+        return restaurantRepository.findAll();
+    }
+
+    public boolean deleteRestaurant(int id) {
+        Optional<Restaurant> optionalRestaurant = getRestaurant(id);
+        if (optionalRestaurant.isPresent()) {
+            restaurantRepository.deleteById(id);
+            return true;
+        } else return false;
+    }
+}
