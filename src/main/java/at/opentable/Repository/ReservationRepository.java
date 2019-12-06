0e2ea4ecbb56249f0a10537e2a1f919659b95efc
okanshn <55379413+okanshn@users.noreply.github.com>
2019-12-06 11:00:37 +0100

    done repositorycontroller

--- src/main/java/at/opentable/Repository/ReservationRepository.java
+++ src/main/java/at/opentable/Repository/ReservationRepository.java
@@ -2,17 +2,22 @@ package at.opentable.repository;
 
 import at.opentable.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
 
-    public Boolean checkTablaAvailability();
+    @Query(value = "")
+    public Boolean checkTeburuAvailability();
 
+    @Query(value="SELECT * FROM reservation WHERE id = ?1", nativeQuery = true)
     public Reservation findByCustomerId(int id);
 
+    @Query(value="")
     public Reservation findByRestaurantDate(Date restaurantDate);
 
+    @Query(value="")
     public Reservation findByCustomerDate(Date customerDate);
 
 
