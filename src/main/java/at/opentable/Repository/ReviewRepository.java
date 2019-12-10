87948852e3853ff80f1a3c764fea9e7aac291890
okanshn <55379413+okanshn@users.noreply.github.com>
2019-12-10 12:03:59 +0100

    fixing

--- src/main/java/at/opentable/Repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package at.opentable.Repository;
-
-import at.opentable.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
