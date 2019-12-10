24e3819b8fe0b9cc1f4fe272dc02b97b3b844509
okanshn <55379413+okanshn@users.noreply.github.com>
2019-12-10 09:58:11 +0100

    reservationrepo finish

--- src/main/java/at/opentable/repository/ReservationRepository.java
+++ src/main/java/at/opentable/repository/ReservationRepository.java
@@ -4,22 +4,25 @@ import at.opentable.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Timestamp;
 import java.util.Date;
 
-public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 
-    @Query(value = "")
-    public Boolean checkTeburuAvailability();
+    @Query(value = "SELECT * FROM reservation WHERE start_date_time >= ?1 AND end_date_time < ?2 AND table_id = ?3", nativeQuery = true)
+    Reservation checkReservationRepo (Timestamp timeStart, Timestamp timeEnd, int teburuId);
 
-    @Query(value="SELECT * FROM reservation WHERE id = ?1", nativeQuery = true)
-    public Reservation findByCustomerId(int id);
+    @Query(value="SELECT ?1 FROM teburu  WHERE restaurant_id = ?2",nativeQuery = true)
+    Boolean checkTeburuAvailibility(int teburuId,int restaurantId);
+    //die Tabelle reservation anstatt teburu abfragen?
 
-    @Query(value="")
-    public Reservation findByRestaurantDate(Date restaurantDate);
+    @Query(value="SELECT * FROM reservation WHERE id = ?1",nativeQuery = true)
+    Reservation findByCustomerId(int id);
 
-    @Query(value="")
-    public Reservation findByCustomerDate(Date customerDate);
+    @Query(value="SELECT * FROM reservation WHERE start_date_time >= ?1 AND end_date_time < ?2",nativeQuery = true)
+    Reservation findByRestaurantDate(Date dateStart, Date dateEnd); // Time anstatt Date vielleicht?
 
-
+    @Query(value="SELECT * FROM reservation WHERE start_date_time >= ?1 AND end_date_time < ?2 AND customer_id = ?3",nativeQuery = true)
+    Reservation findByCustomerDate(Date dateStart, Date dateEnd, int customerId);
 
 }
