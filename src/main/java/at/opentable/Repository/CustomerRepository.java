226c95ec66903a0507ad82dd77a7fa1a7d8e91df
okanshn <55379413+okanshn@users.noreply.github.com>
2019-12-11 09:01:30 +0100

    Customers fixed
    Deleted 2nd CustomerRepo
    
    Changed the Interface of CustomerRepositroy from CrudRepo to JpaRepo
    
    Fixed the Names

--- src/main/java/at/opentable/Repository/CustomerRepository.java
+++ src/main/java/at/opentable/Repository/CustomerRepository.java
@@ -1,9 +1,11 @@
 package at.opentable.repository;
 
 import at.opentable.entity.Customer;
-import at.opentable.entity.Teburu;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
 }
