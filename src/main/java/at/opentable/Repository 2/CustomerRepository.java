dc4bea3303157c6fc5e61a51759402a93559a2d8
elnino07 <55378895+elnino07@users.noreply.github.com>
2019-12-12 15:28:15 +0100

    fixed

--- src/main/java/at/opentable/Repository 2/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package at.opentable.repository;
-
-import at.opentable.entity.Customer;
-import at.opentable.entity.Teburu;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
