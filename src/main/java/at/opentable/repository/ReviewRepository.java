fbfc90a96467e7d9ff893ea35bcaabae7c126240
fabian-s-123 <55379268+fabian-s-123@users.noreply.github.com>
2019-12-17 11:17:38 +0100

    Âcomplete repositories

--- src/main/java/at/opentable/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package at.opentable.repository;
-
-import at.opentable.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
