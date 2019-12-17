a630b436b0138da49082afdf49228ccfa1a0b0e4
okanshn <55379413+okanshn@users.noreply.github.com>
2019-12-17 15:01:39 +0100

    adminrepo finish

---
+++ src/main/java/at/opentable/Repository/AdminRepository.java
@@ -0,0 +1,25 @@
+package at.opentable.Repository;
+
+import at.opentable.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+
+    @Query(value="SELECT * FROM admin WHERE restaurant_id = ?1",nativeQuery = true)
+    Iterable<Admin> findByRestaurantId(int id);
+
+    @Query(value="SELECT * FROM admin WHERE is_owner = 1",nativeQuery = true)
+    Iterable<Admin> findByOwnerAbility();
+
+    @Query(value="SELECT * FROM admin WHERE restaurant_id = ?1 AND is_owner = 1",nativeQuery = true)
+    Admin findByRestaurantOwnerAbility(int id);
+
+
+
+
+
+}
