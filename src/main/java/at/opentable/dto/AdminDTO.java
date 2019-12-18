6a65fed8da28f976ba875aa0191ac4583d6c07ef
okanshn <55379413+okanshn@users.noreply.github.com>
2019-12-18 08:55:22 +0100

    update admin dto

--- src/main/java/at/opentable/dto/AdminDTO.java
+++ src/main/java/at/opentable/dto/AdminDTO.java
@@ -10,6 +10,69 @@ public class AdminDTO {
     private String email;
     private Boolean isOwner;
 
+    public AdminDTO(int id, String firstName, String lastName, String password, int restaurantId, String email, Boolean isOwner) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.restaurantId = restaurantId;
+        this.email = email;
+        this.isOwner = isOwner;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getOwner() {
+        return isOwner;
+    }
+
+    public void setOwner(Boolean owner) {
+        isOwner = owner;
+    }
 }
