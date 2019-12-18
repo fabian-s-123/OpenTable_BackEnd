f39cbec4add206774de1df6ab79ce5cf23744497
okanshn <55379413+okanshn@users.noreply.github.com>
2019-12-18 08:36:20 +0100

    admin dto

---
+++ src/main/java/at/opentable/dto/AdminDTO.java
@@ -0,0 +1,15 @@
+package at.opentable.dto;
+
+public class AdminDTO {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int restaurantId;
+    private String email;
+    private Boolean isOwner;
+
+
+
+}
